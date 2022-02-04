d9f3762a3759b4e412144e528d0fc4e484acd6b4
Jeevan Revaneppa Hirethanad <52557897+jeevan-revaneppa-hirethanad@users.noreply.github.com>
2022-02-04 11:44:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ if __name__ == "__main__":
     app.config['SESSION_TYPE'] = 'filesystem'
 
     sess.init_app(app)
-    app.run(debug=True,port=8888)
+    app.run(host ='0.0.0.0', debug=True,port=8888)
